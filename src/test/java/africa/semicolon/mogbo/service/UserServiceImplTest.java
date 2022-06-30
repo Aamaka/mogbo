c2ef376ecf665f523729a2ca72e2d533f86bef7a
Chiamaka Ezeirunne <fayhvor@gmail.com>
2022-06-30 09:39:33 +0100

    motigbo

--- src/test/java/africa/semicolon/mogbo/service/UserServiceImplTest.java
+++ src/test/java/africa/semicolon/mogbo/service/UserServiceImplTest.java
@@ -4,7 +4,6 @@ import africa.semicolon.mogbo.data.repository.UserRepository;
 import africa.semicolon.mogbo.dto.requests.RegisterUserRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
